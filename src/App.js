56b246ffaa4f73837b7407c7891864100431255f
MauLight <maulisseluz@gmail.com>
2023-03-24 05:09:32 -0300

    basic structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import Heading from './components/Heading';
+import Section from './components/Section';
+
+
+function Page() {
+  return (
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Section>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Section>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Section>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Section>
+              <Heading level={5}>Sub-sub-sub-heading</Heading>
+              <Heading level={5}>Sub-sub-sub-heading</Heading>
+              <Heading level={5}>Sub-sub-sub-heading</Heading>
+              <Section>
+                <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+                <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+                <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+              </Section>
+            </Section>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Page />
     </div>
   );
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,7 @@
+export default function Section({ children }) {
+    return (
+        <section className="section border mx-5 p-3">
+            {children}
+        </section>
+    );
+};
